414b88f5b43e1fca9f758957cdb32e46f66df128
MazenTarek7 <56880548+MazenTarek7@users.noreply.github.com>
2020-10-29 14:00:36 +0200

    Add files via upload
    SPOJ - SMPSUM - Iterated sums

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a , b , sum = 0;
+    cin >> a >> b;
+    for (int i = a ; i <= b ; i++ ){
+        sum = sum + i*i;
+    }
+    cout << sum;
+return 0;
+}
